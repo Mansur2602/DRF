2fa53abce1ffff011b584ba6befe9582fe2e394d
Кали Карим <kalikarim@MacBook-Pro-Kali.local>
2025-02-22 12:19:58 +0500

    Пост-запрос

--- DRF_app/urls.py
+++ DRF_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import book_api
+from .views import book_api, book_api2
 urlpatterns = [
-    path('', book_api, name='book_api')
+    path('', book_api, name='book_api'),
+    path('book_api2', book_api2, name="book_api2")
 ]
--- DRF_app/views.py
+++ DRF_app/views.py
@@ -2,9 +2,30 @@ from django.shortcuts import render
 from .ser import BookSerializer
 from django.http import JsonResponse
 from .models import Book
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view( [ 'GET', 'POST'] )
 
 def book_api(request):
      if request.method == 'GET':
         data = Book.objects.all() 
         ser  = BookSerializer( data, many=True )
         return JsonResponse( ser.data, safe=False )
+
+@api_view(['GET', 'POST'])
+def book_api2(request):
+    match request.method:
+        case 'GET':
+            data = Book.objects.all()
+            ser = BookSerializer(data, many=True)
+            return Response(ser.data)
+        case 'POST':
+            ser = BookSerializer(data=request.data)
+            if ser.is_valid():
+               ser.save()
+            data = Book.objects.all()  
+            ser = BookSerializer(data, many=True)
+            return Response(ser.data)
+         
+
+
